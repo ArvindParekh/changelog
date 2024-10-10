c94f5f39e838e237a38a6b8a9c9f4ad0ceaacbd5
Arvind <aruparekh@gmail.com>
2024-10-10 18:02:54 +0530

    add nextAuth for add log page

--- app/new-log/page.tsx
+++ app/new-log/page.tsx
@@ -1,8 +1,28 @@
+"use client";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/authOptions";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
 export default function AddLog() {
+   const session = useSession();
+   const router = useRouter();
+
+   function handlelogin() {
+      router.push("/api/auth/signin");
+   }
    return (
-      <main className='flex min-h-screen items-center justify-center border flex-col p-5'>
-         <input className='dark:text-black' type='date'></input>
-         <input className='dark:text-black' type='datetime-local'></input>
-      </main>
+      <div>
+         {session.status === "authenticated" ? (
+            <main className='flex min-h-screen items-center justify-center border flex-col p-5'>
+               <input className='dark:text-black' type='date'></input>
+               <input className='dark:text-black' type='datetime-local'></input>
+            </main>
+         ) : (
+            <button onClick={() => handlelogin()}>LogIn</button>
+         )}
+      </div>
    );
 }
