26f1bd0e0d8b9dc71967c1429e8852f440fbe687
Arvind <aruparekh@gmail.com>
2024-10-19 12:37:41 +0530

    feat(ui): implement timeline

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { Timeline } from "./components/ui/timeline";
 
 export default function Home() {
    const [data, setData] = useState<object[]>([]);
@@ -20,27 +21,29 @@ export default function Home() {
       loadData();
    }, []);
 
-   return (
-      <main className='flex min-h-screen flex-col items-start p-5'>
-         {data.toReversed().map((changelog, index) => {
-            return (
-               <div key={index} className='changelog-entry'>
-                  <span className='text-yellow'>{changelog.date}:</span>{" "}
-                  <span className='text-yellow-500'>{changelog.text}</span>
-               </div>
-            );
-         })}
+   return <Timeline data={data} />;
 
-         {session.status === "authenticated" ? (
-            <Link
-               href={"/new-log"}
-               className='p-2 bg-white text-black rounded-lg absolute bottom-5 right-5'
-            >
-               New log
-            </Link>
-         ) : (
-            ""
-         )}
-      </main>
-   );
+   // return (
+   //    <main className='flex min-h-screen flex-col items-start p-5'>
+   //       {data.toReversed().map((changelog, index) => {
+   //          return (
+   //             <div key={index} className='changelog-entry'>
+   //                <span className='text-yellow'>{changelog.date}:</span>{" "}
+   //                <span className='text-yellow-500'>{changelog.text}</span>
+   //             </div>
+   //          );
+   //       })}
+
+   //       {session.status === "authenticated" ? (
+   //          <Link
+   //             href={"/new-log"}
+   //             className='p-2 bg-white text-black rounded-lg absolute bottom-5 right-5'
+   //          >
+   //             New log
+   //          </Link>
+   //       ) : (
+   //          ""
+   //       )}
+   //    </main>
+   // );
 }
