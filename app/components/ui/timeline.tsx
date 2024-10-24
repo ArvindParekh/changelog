2d83ab162e1128f6a2ff8eff2d82234e2cfb5036
Arvind <aruparekh@gmail.com>
2024-10-24 14:53:29 +0530

    fix: image fits the container

--- app/components/ui/timeline.tsx
+++ app/components/ui/timeline.tsx
@@ -74,7 +74,7 @@ export const Timeline = () => {
             </p>
          </div>
 
-         <div ref={ref} className='relative max-w-7xl mx-auto pb-20'>
+         <div ref={ref} className='relative max-w-7xl mx-auto pb-20 bg-black'>
             {data.toReversed().map((item, index) => (
                <div
                   key={index}
@@ -102,9 +102,10 @@ export const Timeline = () => {
                               <Image
                                  src={item.image.imageUrl}
                                  alt='startup template'
+                                 layout='responsive'
                                  width={500}
                                  height={500}
-                                 className='rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
+                                 className='rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
                               />
                            ) : (
                               ""
