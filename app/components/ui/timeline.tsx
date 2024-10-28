8fd6fcb72e1eca1fbd1f0c5bc868698d1db19270
Arvind <aruparekh@gmail.com>
2024-10-28 12:26:21 +0530

    fix: remove embeds from timeline as still WIP

--- app/components/ui/timeline.tsx
+++ app/components/ui/timeline.tsx
@@ -139,22 +139,23 @@ export const Timeline = () => {
                                             />
                                          </div>
                                       )
-                                    : item.media.isEmbedAvailable && (
-                                         <div
-                                            key={idx}
-                                            className='embed-container'
-                                         >
-                                            <iframe
-                                               src={mediaItem.url}
-                                               title='embed media item'
-                                               className='rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
-                                               width='100%'
-                                               height='100%'
-                                               allow='encrypted-media'
-                                               frameBorder='0'
-                                            />
-                                         </div>
-                                      );
+                                    : // item.media.isEmbedAvailable && (
+                                      //      <div
+                                      //         key={idx}
+                                      //         className='embed-container'
+                                      //      >
+                                      //         <iframe
+                                      //            src={mediaItem.url}
+                                      //            title='embed media item'
+                                      //            className='rounded-lg shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
+                                      //            width='100%'
+                                      //            height='100%'
+                                      //            allow='encrypted-media'
+                                      //            frameBorder='0'
+                                      //         />
+                                      //      </div>
+                                      //   );
+                                      "";
                               })}
                            </div>
                         )}
